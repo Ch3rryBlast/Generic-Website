65e1a30b48c86fad168b1b2094d2995b744a5310
Ch3rryBlast <charanthoka@gmail.com>
2026-02-28 08:21:55 -0600

    er

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+from flask import Flask, session, redirect, render_template, request
+from flask_session import Session
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
